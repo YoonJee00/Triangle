d8911c7cd2da649e57da1ac724468b363f6d9505
YoonJee00 <bestjang00@gmail.com>
2023-03-29 09:56:16 +0900

    삼각형 무게중심

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
+// 강사님이 풀어주신 문제
+
+import java.util.Scanner;
+
+class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void print() {
+        System.out.println(x + " " + y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+class Triangle {
+    private Point[] p;
+
+    public Triangle(int[] param) {
+        p = new Point[3];
+        for (int i = 0, j = 0; i < p.length; i++, j += 2) {
+            p[i] = new Point(param[j], param[j + 1]);
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < p.length; i++) {
+            p[i].print();
+        }
+    }
+
+    public void getCoG() {
+        int x = 0, y = 0;
+        for (int i = 0; i < p.length; i++) {
+            x += p[i].getX();
+            y += p[i].getY();
+        }
+
+        System.out.printf("(%.1f, %.1f)\n", (double) x / 3, (double) y / 3);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int[] p = new int[6];
+        for (int i = 0; i < p.length; i++) {
+            p[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Triangle tri = new Triangle(p);
+        tri.getCoG();
+
     }
+
 }
